643545d2c3695b3a497840437dc30a6589cb12d3
Joseph Dennis <dennis.joseph@sdworx.com>
2021-10-22 12:33:09 +0100

    Added load/save database buttons.

---
+++ MultiSql/Converters/CountToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MultiSql.Converters
+{
+    [ValueConversion(typeof(Int32), typeof(Visibility))]
+    public class CountToVisibilityConverter : IValueConverter
+    {
+
+        public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(Visibility))
+            {
+                throw new InvalidOperationException("The target must be a boolean");
+            }
+
+            return value != null && value.GetType() == typeof(Int32) && (Int32) value > 0
+                       ? Visibility.Visible
+                       : Visibility.Collapsed;
+        }
+
+        public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture) => throw new NotImplementedException();
+
+    }
+}
