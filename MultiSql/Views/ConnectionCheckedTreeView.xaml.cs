db7cd33013c9847bd23b6aa66c6661fca70c5a9f
Dennis Joseph <wizden@gmail.com>
2021-11-15 12:57:54 +0000

    Ensure password object null error does not occur.

---
+++ MultiSql/Views/ConnectionCheckedTreeView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultiSql.Views
+{
+    /// <summary>
+    /// Interaction logic for ConnectionCheckedTreeView.xaml
+    /// </summary>
+    public partial class ConnectionCheckedTreeView : UserControl
+    {
+        public ConnectionCheckedTreeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
